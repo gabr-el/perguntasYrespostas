65c3986c6752c1a9592dae69a2f7627699cd6a5e
gabr-el <58999438+gabr-el@users.noreply.github.com>
2024-07-23 09:26:59 -0300

    Atualizar o Pergunta.js

--- database/Pergunta.js
+++ database/Pergunta.js
@@ -1,5 +1,5 @@
 //Definindo um model importando o sequelize e a conexao com o banco de dados
-const { DataTypes } = require("sequelize");
+const { Sequelize, DataTypes } = require("sequelize");
 const connection = require("./database");
 
 //Definindo a tabela e seus campos
